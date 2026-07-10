681b1554cf502f26bd137d0cf908399d7b9bb465
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2026-07-10 17:23:03 -0300

    30 min cache ttl

--- cmd/main.go
+++ cmd/main.go
@@ -276,7 +276,7 @@ func main() {
 	}
 	defer authCache.Cleanup()
 
-	resourceCache, err := inmemoryCache.NewResourceCache(5 * time.Minute)
+	resourceCache, err := inmemoryCache.NewResourceCache(30 * time.Minute)
 	if err != nil {
 		setupLog.Error(err, "unable to start resource cache")
 		os.Exit(1)
